dc9f4dd3a048cabc9db070744eebb382eb900d40
Katerina_Kravets <12d7v38y@gmail.com>
2025-07-17 22:59:52 +0300

    add index.html and add js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,46 @@
-<form class="feedback-form"></form>
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: ''
+};
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  } catch (error) {
+    console.error('Помилка при зчитуванні даних із localStorage:', error);
+  }
+}
+
+
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Дані з форми:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
+
 
 
